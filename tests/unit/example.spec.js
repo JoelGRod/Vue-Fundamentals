1bb8554164d60e412c99f2a7d8b732e3fea00dd7
joelgrod <joelgrod@gmail.com>
2021-08-12 18:28:27 +0100

    example test suite added

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,26 @@
-import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      props: { msg }
-    })
-    expect(wrapper.text()).toMatch(msg)
+
+// Test suite
+describe("Example Component", () => {
+
+  test("Must be greater than 10", () => {
+    
+    // Arrange
+    let value = 10;
+
+    // Act
+    value = value + 2;
+
+    // Assert
+    expect(value).toBeGreaterThan(10);
+
+    // Not this way... ------
+    // if(value > 10) {
+    //   // All ok
+    // } else {
+    //   throw new Error(`${value} is not greater than 10`)
+    // }
+
   })
+
 })
