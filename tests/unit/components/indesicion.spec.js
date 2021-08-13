5be640255367f3e8613a6efa13c4af1db619231b
joelgrod <joelgrod@gmail.com>
2021-08-13 14:43:49 +0100

    indesicion component first test added

---
+++ tests/unit/components/indesicion.spec.js
@@ -0,0 +1,14 @@
+import { shallowMount, mount } from '@vue/test-utils';
+import Indesicion from '@/components/Indesicion';
+
+describe('Indesicion Component', () => {
+
+    let wrapper;
+    beforeEach(() => {
+        wrapper = shallowMount( Indesicion );
+    });
+
+    test('should ', () => {
+        
+    });
+});
