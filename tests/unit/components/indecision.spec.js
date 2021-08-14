22c2cbefdfc6384f77c3ac838740699d2b94904e
JoelGRod <joelgrod@gmail.com>
2021-08-14 01:13:04 +0100

    indecision tests definition

--- tests/unit/components/indecision.spec.js
+++ tests/unit/components/indecision.spec.js
@@ -9,6 +9,22 @@ describe('Indesicion Component', () => {
     });
 
     test('should match with the snapshot', () => {
-        expect(wrapper.html()).toMatchSnapshot();
+        expect( wrapper.html() ).toMatchSnapshot();
+    });
+
+    test('write something in the input shouldn\'t show anything (console.log)', () => {
+        
+    });
+
+    test('write ? shold run fetch request', () => {
+        
+    });
+
+    test('test on getAnswer', () => {
+        
+    });
+
+    test('test on getAnswer - API fails', () => {
+        
     });
 });
