c7ca990a9e9dd18d80e7070afdef45406576d8bd
joelgrod <joelgrod@gmail.com>
2021-08-14 17:57:45 +0100

    indecision component api tests, getAnswer spy and fetch resolve mock

--- tests/unit/components/indecision.spec.js
+++ tests/unit/components/indecision.spec.js
@@ -3,28 +3,76 @@ import Indecision from '@/components/Indecision';
 
 describe('Indesicion Component', () => {
 
-    let wrapper;
+    let wrapper; // Wrapper
+    let clgSpy; // Spy
+    // Mock - IMPORTANT EXAMPLE!
+    global.fetch = jest.fn(() => Promise.resolve({
+        json: () => Promise.resolve({
+            answer: 'yes',
+            forced: false,
+            image:  'https://yesno.wtf/assets/yes/2.gif'
+        })
+    }));
+    // LifeCycle
     beforeEach(() => {
-        wrapper = shallowMount( Indecision );
+
+        wrapper = shallowMount(Indecision);
+        clgSpy = jest.spyOn(console, 'log');
+
+        jest.clearAllMocks(); // Restart everything
+
     });
 
+    // SnapShot
     test('should match with the snapshot', () => {
-        expect( wrapper.html() ).toMatchSnapshot();
+
+        expect(wrapper.html()).toMatchSnapshot();
+
     });
 
-    test('write something in the input shouldn\'t show anything (console.log)', () => {
-        
+    // Spy & Mocks
+    test('write something in the input without ? character shouldn\'t show anything (console.log)', async () => {
+
+        // Arrange -> wrapper
+        const getAnswerSpy = jest.spyOn(wrapper.vm, 'getAnswer'); // Spy
+        // Act
+        const input = wrapper.find('input');
+        await input.setValue('Hello World');
+        // Assert
+        expect(clgSpy).toHaveBeenCalledTimes(1);
+        expect(getAnswerSpy).toHaveBeenCalledTimes(0);
+
     });
 
-    test('write ? shold run fetch request', () => {
-        
+    // Spy & Mocks - We need a fetch mock here
+    test('write ? shold run getAnswer', async () => {
+
+        // Arrange
+        const getAnswerSpy = jest.spyOn(wrapper.vm, 'getAnswer'); // Spy
+        // Act
+        const input = wrapper.find('input');
+        await input.setValue('Hello Word?');
+        // Assert
+        expect(clgSpy).toHaveBeenCalledTimes(1);
+        expect(getAnswerSpy).toHaveBeenCalledTimes(1);
+
     });
 
-    test('test on getAnswer', () => {
-        
+    // Fetch Mock, simulates api response
+    test('test on getAnswer', async () => {
+
+        // Arrange - Wrapper, Fetch Mock
+        // Act
+        await wrapper.vm.getAnswer();
+        const img = wrapper.find('img');
+        // Assert
+        expect( img.exists() ).toBeTruthy();
+        expect( wrapper.vm.answer ).toBe('Si');
+        expect( wrapper.vm.image ).toBe('https://yesno.wtf/assets/yes/2.gif');
+
     });
 
     test('test on getAnswer - API fails', () => {
-        
+
     });
 });
